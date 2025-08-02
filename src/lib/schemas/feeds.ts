14f0cd38d0cfe216b3a64c825c63964ba4f5ad76
Shintaro Jokagi <taroj1205@gmail.com>
2025-08-03 11:51:22 +1200

    fix(feeds): add proper schema for feeds

--- src/lib/schemas/feeds.ts
+++ src/lib/schemas/feeds.ts
@@ -13,4 +13,20 @@ export const FeedSchema = z.object({
   platform: PlatformSchema,
 })
 
+export const FeedsResponseSchema = z.array(FeedSchema)
+
+export const AddFeedUrlRequestSchema = z.object({
+  url: z.string().min(1),
+})
+
+export const AddCustomFeedRequestSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().min(1),
+  imageUrl: z.string().nullable().optional(),
+  url: z.string().nullable().optional(),
+})
+
 export type Feed = z.infer<typeof FeedSchema>
+export type FeedsResponse = z.infer<typeof FeedsResponseSchema>
+export type AddFeedRequest = z.infer<typeof AddFeedUrlRequestSchema>
+export type AddCustomFeedRequest = z.infer<typeof AddCustomFeedRequestSchema>
