7fe8ffe89e5d86d97caaf2e012032f5201bdc663
Shintaro Jokagi <taroj1205@gmail.com>
2025-08-03 16:25:57 +1200

    feat(metadata): add metadata

--- src/app/(auth)/search/page.tsx
+++ src/app/(auth)/search/page.tsx
@@ -1,8 +1,14 @@
 import { Suspense } from "react"
 import { SearchSection } from "@/components/client"
 import { SearchLoadingSkeleton } from "@/components/skeletons/search-loading-skeleton"
+import { Metadata } from "next"
 
 export const dynamic = "force-static"
+
+export const metadata: Metadata = {
+  title: "Search",
+  description: "Search through all posts in your feeds",
+}
 
 export default function SearchPage() {
   return (
